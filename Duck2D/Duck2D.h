706c26f27894e150ba988b42819439c37255dd06
Dani-24 <dani.toledo.23@hotmail.com>
2021-11-23 13:13:16 +0100

    Add game base
    - wowadahwid let's do a clone of Worms

---
+++ Duck2D/Duck2D.h
@@ -0,0 +1,12 @@
+#ifndef DUCK2D_H
+#define DUCK2D_H
+
+// Quack
+
+#include "configuration/dMath.h"
+
+#include "collisions/dCollisions.h"
+
+#include "dynamics/dBody.h"
+
+#endif
---
+++ GameToTestDuck2D/ModuleScene.cpp
@@ -0,0 +1,38 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleScene.h"
+
+ModuleScene::ModuleScene(Application* app, bool start_enabled) : Module(app, start_enabled)
+{
+	graphics = NULL;
+}
+
+ModuleScene::~ModuleScene()
+{}
+
+// Load assets
+bool ModuleScene::Start()
+{
+	LOG("Loading Intro assets");
+	bool ret = true;
+
+	App->renderer->camera.x = App->renderer->camera.y = 0;
+
+	return ret;
+}
+
+// Load assets
+bool ModuleScene::CleanUp()
+{
+	LOG("Unloading Intro scene");
+
+	return true;
+}
+
+// Update: draw background
+update_status ModuleScene::Update()
+{
+	
+
+	return UPDATE_CONTINUE;
+}
